2dc5581d02cddeeaec544cccf40feec40ccccd72
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2022-10-26 14:07:27 +0900

    added scripts

--- load_fcz.py
+++ load_fcz.py
@@ -1,10 +1,14 @@
 import foldcomp
-import cmd from pymol
-def load_fcz(filename, object=None, state=0, *, _self=cmd):
+from pymol import cmd
+import glob
+
+def load_fcz(filename):
     with open(filename, "rb") as fcz:
         fcz_binary = fcz.read()
         (name, pdb) = foldcomp.decompress(fcz_binary)
-        if (object == None):
-            object = name
-        cmd.read_pdbstr(pdb, object)
+        print(name)
+        cmd.read_pdbstr(pdb,name)
 
+pymol.cmd.extend("load_fcz", load_fcz)
+names_filenames_sc = lambda: cmd.Shortcut(cmd.get_names() + glob.glob('*'))
+cmd.auto_arg[0]['load_fcz'] = [names_filenames_sc, 'filename or object name', '']
